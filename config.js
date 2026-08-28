08e6d7b283c665158d55dbda7373d598a28e2f62
m891002-boop <m891002@gmail.com>
2026-08-28 15:28:32 +0800

    Set GOOGLE_SCRIPT_URL to the deployment URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script 部署完成後，將網址貼在引號中。
 // 例如：window.GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/XXXXXXXX/exec";
-window.GOOGLE_SCRIPT_URL = "";
+window.GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwe6cFYxxUf34RvHqSnf9OeqUK1GGjzSMSH53WMsTSm96sqjY6Q1QRdU74l3Xx2E_dqgg/exec";
